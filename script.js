2d6b8e04f7c9aea30c5e69aeee004f020f23c63b
Brent van Malsen <i507960@student.fontys.nl>
2024-01-05 20:42:42 +0100

    Created a workaround to solve my page resize problem

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// Voorkom dat het venster kan worden vergroot of verkleind
+// Prevent the window from being enlarged or reduced.
 window.addEventListener('resize', function () {
-    window.resizeTo(400, 600);
+    window.resizeTo(400, 662);
 });
+// I did 400x662 because my code makes a 400x600 window, however
+// when i resize the windows it automatically shrunk in vertical size just
+// a little bit. This is why I made the resizeTo 62 pixels bigger than
+// the original size of the window to compensate the hight loss.
